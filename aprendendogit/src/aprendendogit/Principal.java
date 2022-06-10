b49221c1a87a59c66c6c35203a4f44168201a8d2
Rafademetrio <rafademetrio@gmail.com>
2022-06-10 17:34:01 -0300

    terceiro commit

--- aprendendogit/src/aprendendogit/Principal.java
+++ aprendendogit/src/aprendendogit/Principal.java
@@ -3,7 +3,7 @@ package aprendendogit;
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Agora mudou para versão 2.0");
+		System.out.println("Agora mudou para versão 3.0");
 	}
 
 }
